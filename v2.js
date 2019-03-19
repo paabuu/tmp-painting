aba76f55338c87d13bb904b716d8fcb5c608d2f5
Wenjie Yang <wenjie.yang@ihandysoft.com>
2019-03-19 20:03:40 +0800

    save

--- v2.js
+++ v2.js
@@ -8,7 +8,7 @@ function GetSvg(svgText) {
     const width = +svg.getAttribute('width').replace('px', '');
     const height = +svg.getAttribute('height').replace('px', '');
     const clientWidth = document.body.clientWidth;
-    const ratio = width / clientWidth;
+    const ratio = width / clientWidth * 2;
     const w = clientWidth;
     const h = height / ratio;
 
@@ -20,9 +20,11 @@ function GetSvg(svgText) {
 
     $('#touch').innerHTML = '11'
 
-    $$('svg .white').forEach(p => {
+    $$('svg path').forEach(p => {
         p.addEventListener('click', () => {
-            p.setAttribute('style', 'fill: red;')
+            const fill = p.getAttribute('fill');
+            if (fill === 'black' || !fill) return;
+            p.setAttribute('fill', color);
 
             const cloneNode = svg.cloneNode(true);
             cloneNode.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
@@ -33,14 +35,19 @@ function GetSvg(svgText) {
         })
     });
 
+    /**
+     * @param {scale}: 缩放比例
+     * @param {pan}
+     */
+
     let scale = 1;
     let pan = {x: 0, y: 0};
     let dx = 0;
     let dy = 0;
     let origin = {x: w / 2, y: h / 2};
     let point = {x: w / 2, y: h / 2};
+    let color = 'red';
 
-    // svg.style.transform = `matrix(1.2, 0, 0, 1.2, 0, 0)`;
     var mc = new Hammer.Manager(svg);
     var panInstance = new Hammer.Pan({
         threshold: 2,
@@ -78,7 +85,7 @@ function GetSvg(svgText) {
                 break;
             case 'pinchout':
                 scale += .03;
-                svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${pan.x}, ${pan.y})`;
+                 svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${pan.x}, ${pan.y})`;
                 break;
         }
 
@@ -89,15 +96,32 @@ function GetSvg(svgText) {
         // 原点在contianer的位置
         try {
             const {x, y} = e.center;
-            $('#touch').innerHTML = `${x}-${y}-${scale}`
-            origin.x += (x - point.x) / scale;
-            origin.y += (y - point.y) / scale;
+            origin.x += (Math.abs(dx) + x - point.x) / scale;
+            origin.y += (Math.abs(dy) + y - point.y) / scale;
+            $('#touch').innerHTML = `${x}-${origin.x} ${y}-${origin.y}`;
             point.x = x;
             point.y = y;
-            // const range = {x: scale * x - pan.x, y: scale * y - pan.y};
+
+            if (origin.x > w) {
+                origin.x = w;
+                point.x = w;
+            }
+
+            if (origin.x < 0) {
+                origin.x = 0;
+                point.x = 0;
+            }
+
+            if (origin.y > h) {
+                origin.y = h;
+                point.y = h;
+            }
+
+            if (origin.y < 0) {
+                origin.y = 0;
+                point.y = 0;
+            }
             svg.style.transformOrigin = `${origin.x}px ${origin.y}px`;
-            // $('#touch').style.color = 'red';
-            // $('#touch').innerHTML = `${origin.x}-${origin.y}-${scale}`
         } catch(e) {
             $('#touch').innerHTML = e
         }
@@ -109,20 +133,77 @@ function GetSvg(svgText) {
     });
 
     function boundsCheck() {
-        const range = {x: w * (scale - 1) / 2, y: h * (scale - 1) / 2};
-        const sign = { x: dx === 0 ? 1 : dx / Math.abs(dx), y: dy === 0 ? 1 : dy / Math.abs(dy)};
-        if (Math.abs(dx) >= range.x) {
-            dx = sign.x * range.x;
-        } 
-        if (Math.abs(dy) >= range.y) {
-            dy = sign.y * range.y;
-        } 
+        // 根据对称中心计算左右边界
+        const range = {
+            left: w * (scale - 1) * origin.x / w,
+            right: w * (scale - 1) * (1 - origin.x / w),
+            top: h * (scale - 1) * origin.y / h,
+            bottom: h * (scale - 1) * (1 - origin.y / h)
+        };
 
-        // $('#touch').innerHTML =   `dx: ${dx}, scale: ${scale}, w: ${w}, rangeX: ${range.x}`
+        // 向左划
+        if (dx < 0 && Math.abs(dx) >= range.right) {
+            dx = -range.right;
+        }
+        // 向右划
+        if (dx > 0 && Math.abs(dx) >= range.left) {
+            dx = range.left;
+        }
+        // 向上划
+        if (dy < 0 && Math.abs(dy) >= range.bottom) {
+            dy = -range.bottom;
+        }
+        // 向下划
+        if (dy > 0 && Math.abs(dy) >= range.top) {
+            dy = range.top;
+        }
+
         svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${dx}, ${dy})`;
+
+        if (scale === 1) {
+            svg.style.transform = `matrix(1, 0, 0, 1, 50%, 50%)`;
+            dx = 0;
+            dy = 0;
+            origin = { x: w / 2, y: h / 2 };
+            point = { x: w /2, y: h / 2 };
+        }
     }
 
-    console.log(mc)
+    const COLORS = [
+        "be8c65",
+        "d7d9c6",
+        "e5e3d5",
+        "918474",
+        "dec99d",
+        "c2a27f",
+        "4d3116",
+        "716b4d",
+        "f9f8f6",
+        "be5937",
+        "b2af9b",
+        "e2d6bd",
+        "ac9a83",
+        "c6cbb8",
+        "b0b885",
+        "cec26d",
+        "cab493",
+        "775838",
+        "efece3",
+        "b1754e"
+    ];
+
+    COLORS.forEach(c => {
+        console.log(c)
+        console.log($('.colors'))
+        const ele = document.createElement('p');
+        ele.innerHTML = c;
+        ele.setAttribute('class', 'color');
+        ele.style.backgroundColor = '#' + c;
+        ele.addEventListener('click', function() {
+            color = '#' + c;
+        });
+        $('.colors').appendChild(ele);
+    });
 }
 
 window.onload = function() {
@@ -147,6 +228,7 @@ window.onload = function() {
     });
 }
 
+
 function getDistance(p1, p2) {
     const x = p1.pageX - p2.pageX;
     const y = p1.pageY - p2.pageY;
@@ -155,4 +237,4 @@ function getDistance(p1, p2) {
 
 window.GetSvg = GetSvg;
 
-GetSvg(svgText)
+GetSvg(tarot);
