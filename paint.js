de4991c59b65527adf8570955a1574a1d6c3698c
Wenjie Yang <wenjie.yang@ihandysoft.com>
2019-03-21 21:23:02 +0800

    save

--- paint.js
+++ paint.js
@@ -4,22 +4,65 @@
  * @param {object} colors 色块数据
  */
 function render(svgText, colors) {
-    $('#container').append($(svgText));
-    var clientWidth = document.body.clientWidth;
+    $('#container').empty().append($(svgText));
+    var clientHeight = document.body.clientHeight;
     var svgRatio = $('svg').width() / $('svg').height();
-    var w = clientWidth * 0.5;
-    var h = w / svgRatio;
+    var h = clientHeight * 0.7;
+    var w = h * svgRatio;
     var color = 'white';
+    var progress = {};
+    var HIGHLIGHT_COLOR = '#333';
+
+    colors.forEach(function(c) {
+        var all = $('.color-' + c).length;
+        var now = $('.color-' + c).filter(function(i, c) {return !!$(c).attr('painted')}).length;
+        progress[c] = { all: all, now: now };
+    });
 
     // 添加涂色事件
     $('svg').attr('width', w).attr('height', h);
     $('svg path').each(function(index, path) {
         $(path).on('touchstart', function() {
             var classname = $(this).attr('class');
-            console.log(classname, color);
-            if (!classname || classname.indexOf('paintable') < 0 || classname.indexOf(color.slice(1)) < 0) return;
+            if(!classname || classname.indexOf('paintable') < 0 || classname.indexOf(color.slice(1)) < 0 || !!$(this).attr('painted')) return;
             $(this).attr('fill', color);
-            $(this).attr('painted', 'true');
+            $(this).attr('painted', "true");
+
+            var selectedColor = progress[color.slice(1)];
+            selectedColor.now += 1;
+
+            // 单个颜色完成
+            if(selectedColor.now >= selectedColor.all) {
+                $('.brush-' + color.slice(1)).addClass('brush-finished');
+            }
+
+            // 100%完成
+            if($('.brush-finished').length === colors.length) {
+                try {
+                    window.webkit.messageHandlers.PaintDone.postMessage();
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+
+            // 保存进度
+            var clone = $('svg').clone();
+            // 原始缩放比例
+            clone.css('transform', 'matrix(1, 0, 0, 1, 0, 0)').css('transform-origin', '50% 50%');
+            // 清除未涂完的高亮色
+            clone.find('.paintable').each(function(i, el) {
+                if($(this).attr('fill') == HIGHLIGHT_COLOR) {
+                    $(this).attr('fill', 'white');
+                }
+            });
+            var div = document.createElement('div');
+            $(div).append(clone);
+            window.localStorage.setItem('svg', $(div).html());
+            try {
+                window.webkit.messageHandlers.UpdateSvg.postMessage($(div).html());
+            } catch (e) {
+                console.log(e);
+            }
         });
     });
 
@@ -43,6 +86,7 @@ function render(svgText, colors) {
 
     // 事件管理器
     var svg = document.querySelector('svg');
+    // svg.style.transform = 'matrix(' + minScale + ', 0, 0, ' + minScale + ', 0, 0)'
     var mc = new Hammer.Manager(svg);
     var panInstance = new Hammer.Pan({
         threshold: 2,
@@ -77,7 +121,7 @@ function render(svgText, colors) {
         switch(e.additionalEvent) {
             case 'pinchin':
                 scale -= .05;
-                if (scale < 1) {
+                if(scale < 1) {
                     scale = 1;
                 }
                 svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${pan.x}, ${pan.y})`;
@@ -100,28 +144,28 @@ function render(svgText, colors) {
             point.x = x;
             point.y = y;
 
-            if (origin.x > w) {
+            if(origin.x > w) {
                 origin.x = w;
                 point.x = w;
             }
 
-            if (origin.x < 0) {
+            if(origin.x < 0) {
                 origin.x = 0;
                 point.x = 0;
             }
 
-            if (origin.y > h) {
+            if(origin.y > h) {
                 origin.y = h;
                 point.y = h;
             }
 
-            if (origin.y < 0) {
+            if(origin.y < 0) {
                 origin.y = 0;
                 point.y = 0;
             }
             svg.style.transformOrigin = `${origin.x}px ${origin.y}px`;
         } catch(e) {
-            $('#touch').innerHTML = e
+            console.log(e);
         }
     });
 
@@ -138,24 +182,24 @@ function render(svgText, colors) {
             bottom: h * (scale - 1) * (1 - origin.y / h)
         };
         // 向左划
-        if (dx < 0 && Math.abs(dx) >= range.right) {
+        if(dx < 0 && Math.abs(dx) >= range.right) {
             dx = -range.right;
         }
         // 向右划
-        if (dx > 0 && Math.abs(dx) >= range.left) {
+        if(dx > 0 && Math.abs(dx) >= range.left) {
             dx = range.left;
         }
         // 向上划
-        if (dy < 0 && Math.abs(dy) >= range.bottom) {
+        if(dy < 0 && Math.abs(dy) >= range.bottom) {
             dy = -range.bottom;
         }
         // 向下划
-        if (dy > 0 && Math.abs(dy) >= range.top) {
+        if(dy > 0 && Math.abs(dy) >= range.top) {
             dy = range.top;
         }
         svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${dx}, ${dy})`;
 
-        if (scale === 1) {
+        if(scale === 1) {
             svg.style.transform = `matrix(1, 0, 0, 1, 50%, 50%)`;
             dx = 0;
             dy = 0;
@@ -165,12 +209,21 @@ function render(svgText, colors) {
     }
 
     function renderBrush(colors) {
+        var width = colors.length * 10 + 2; 
+        $('.brushes').css('width', width + 'vh');
         colors.forEach(function(c) {
-            const ele = document.createElement('p');
+            const ele = document.createElement('span');
             ele.innerHTML = c;
-            ele.setAttribute('class', 'color');
+            ele.setAttribute('class', 'brush ' + 'brush-' + c);
             ele.style.backgroundColor = '#' + c;
             ele.addEventListener('click', function() {
+                if(c !== color.slice(1)) {
+                    try {
+                        window.webkit.messageHandlers.ProgressUpdate.ChangePencil('#' + c);
+                    } catch(e) {
+                        console.log(e);
+                    }
+                }
                 color = '#' + c;
 
                 $('.paintable').each(function(index, el) {
@@ -183,33 +236,45 @@ function render(svgText, colors) {
                 });
 
                 $('.color-' + c).each(function(i, el) {
-                    // console.log($(el).attr('painted'))
                     if(!$(el).attr('painted')) {
-                        $(el).attr('fill', '#333');
+                        $(el).attr('fill', HIGHLIGHT_COLOR);
                     }
                 });
+
+                $(this).addClass('brush-selected').siblings().removeClass('brush-selected');
             });
-            $('.colors').append(ele);
+
+            $('.brushes').append(ele);
+
+            // 完成状态
+            var colorProgress = progress[c];
+            if(colorProgress.now === colorProgress.all) {
+                var classname = '.brush-' + c;
+                $(classname).addClass('brush-finished');
+            }
         });
+        
     }
 }
 
 const COLORS = ["c7a480", "7b5733", "ebcc9e", "8b8f78", "bbc8b5", "cf8f57", "fef6e3", "be5937", "4d3116", "cec26d", "a8b5a0", "b18967", "c4b99d", "927f6f", "706a4c", "ddc58a", "b77044", "a3917d", "d6d5c1", "b4a38f"];
 
-render(tarot, COLORS);
+var buffer = window.localStorage.getItem('svg');
+
+render(buffer || tarot, COLORS);
 
 // 阻止页面全局放大
 window.onload = function() {
     // 阻止双击放大
     var lastTouchEnd = 0;
     document.addEventListener('touchstart', function(event) {
-        if (event.touches.length > 1) {
+        if(event.touches.length > 1) {
             event.preventDefault();
         }
     });
     document.addEventListener('touchend', function(event) {
         var now = (new Date()).getTime();
-        if (now - lastTouchEnd <= 300) {
+        if(now - lastTouchEnd <= 300) {
             event.preventDefault();
         }
         lastTouchEnd = now;
