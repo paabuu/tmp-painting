16d4a141c7e2674ae7d5135f3224fc781c2652ef
Wenjie Yang <wenjie.yang@ihandysoft.com>
2019-03-28 15:54:54 +0800

    save

--- paint.js
+++ paint.js
@@ -5,6 +5,10 @@
  */
 function Render(svgText, c) {
     $('#container').empty().append($(svgText));
+    // 点击颜色显示色值的隐藏提示
+    var counting = false;
+    var clickCount = 0;
+
     var svgRatio = 87 / 150;
     var h = $('#container').height() * 0.8;
     var w = h * svgRatio;
@@ -253,10 +257,17 @@ function Render(svgText, c) {
                 if(c !== color.slice(1)) {
                     try {
                         window.webkit.messageHandlers.ChangeColor.postMessage('#' + c);
+                        clickCount = 0;
+                        setTimeout(function() {
+                            if (clickCount > 10 && color === '#' + c) {
+                                alert(c);
+                            }
+                        }, 5000);
                     } catch(e) {
                         console.log(e);
                     }
                 }
+                clickCount++;
                 color = '#' + c;
 
                 $('.paintable').each(function(index, el) {
